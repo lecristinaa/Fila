f86ec9bf718d1d21da0a7be6a030ea550d144d2c
lê <112022669+lecristinaa@users.noreply.github.com>
2024-03-19 11:56:44 -0300

    Create fila.js

---
+++ fila.js
@@ -0,0 +1,37 @@
+// 1º a entrar é 1º a sair
+
+// Fila
+let alunos = [
+    "Mario",
+    "Isabelle",
+    "Caua",
+    "Vitor",
+    "Beatriz",
+]
+
+// Criar fila --> criarFila(tamanho = int):any[] --> retorna qualquer coisa
+// function criarFila(tamanho = 5){
+//     return [... new Array(tamanho)]
+// }   
+// criarFila(alunos)
+console.log(alunos)
+
+
+
+// Inserir na fila --> inserirFila(fila:any[], item):void --> passa dois parâmetros:
+// fila que é uma array de qualquer coisa; e retorna vazio
+function inserirFila(fila, item){ 
+    const posicao = fila.IndexOf[0](undefined)
+    fila[posicao] = item 
+    
+    if(posicao = fila.lastIndexOf(undefined)){
+        
+    }
+}
+
+
+// Retirar da fila --> remover(fila:any[]):any[] --> passa um parâmetro:
+// fila que é uma array de qualquer coisa; e retorna qualquer coisa
+function removerDaFila(fila){ 
+
+}
