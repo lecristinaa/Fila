d2bf4a3dd1e3d3ef8c5f9a0e8aed84ffb5a07400
lê <112022669+lecristinaa@users.noreply.github.com>
2024-03-26 11:53:27 -0300

    Update fila.js

--- fila.js
+++ fila.js
@@ -1,42 +1,44 @@
 // 1º a entrar é 1º a sair
 
 const alunos = [
-    "Mario",
     "Isabelle",
     "Caua",
     "Vitor",
-    "Beatriz",
+    "Mario",
 ]
 
 
 // Criar fila --> criarFila(tamanho = int):any[] --> retorna qualquer coisa
 export function criarFila(tamanho = 5){
-    console.log("Fila criada")
     return [... new Array(tamanho)]
 }   
-criarFila()
-
+let minhaFila = criarFila()
+console.log(minhaFila)
 
 // Inserir na fila --> inserirFila(fila:any[], item):void --> passa dois parâmetros:
 // fila que é uma array de qualquer coisa; e retorna vazio
-export function inserirNaFila(fila, item){ 
-    const posicao = fila.indexOf(undefined)
-        if (posicao === -1) { // Se não tiver espaço na fila
-            console.log("Não tem espaço")
-            return
-        }
-    fila[posicao] = item 
+export function inserirNaFila(fila, item){
+    const posicao = fila.indexOf(undefined) // Confirma se a pilha já não está cheia
+    if(posicao === -1){
+        console.log("Não tem espaço")
+        return
+    }
+    fila[posicao] = item // Caso não esteja, insere na pilha
     return
 }
-inserirNaFila()
+inserirNaFila(minhaFila, "Alessandra")
+console.log(minhaFila)
 
 // Retirar da fila --> remover(fila:any[]):any[] --> passa um parâmetro:
 // fila que é uma array de qualquer coisa; e retorna qualquer coisa
 export function removerDaFila(fila){ 
-    if(fila[0] === undefined){
-        console.log("Fila Vazia")
-        return
+    let primeiro = fila[0]
+    if(primeiro === undefined){
+        for (let i = 0; i < fila.length; i++) {
+            fila[i] = fila[i+1]
+            primeiro = fila[i]
+        }
     }
-    fila.pop([0])
+    console.log(primeiro)
 }
-removerDaFila()
+removerDaFila(minhaFila)
