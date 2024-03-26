261c9ebcef303bb3a21fd5094354f53b8ce1e3e3
lê <112022669+lecristinaa@users.noreply.github.com>
2024-03-25 22:31:03 -0300

    Update fila.js

--- fila.js
+++ fila.js
@@ -1,7 +1,6 @@
 // 1º a entrar é 1º a sair
 
-// Fila
-let alunos = [
+const alunos = [
     "Mario",
     "Isabelle",
     "Caua",
@@ -9,29 +8,35 @@ let alunos = [
     "Beatriz",
 ]
 
-// Criar fila --> criarFila(tamanho = int):any[] --> retorna qualquer coisa
-// function criarFila(tamanho = 5){
-//     return [... new Array(tamanho)]
-// }   
-// criarFila(alunos)
-console.log(alunos)
 
+// Criar fila --> criarFila(tamanho = int):any[] --> retorna qualquer coisa
+export function criarFila(tamanho = 5){
+    console.log("Fila criada")
+    return [... new Array(tamanho)]
+}   
+criarFila()
 
 
 // Inserir na fila --> inserirFila(fila:any[], item):void --> passa dois parâmetros:
 // fila que é uma array de qualquer coisa; e retorna vazio
-function inserirFila(fila, item){ 
-    const posicao = fila.IndexOf[0](undefined)
+export function inserirNaFila(fila, item){ 
+    const posicao = fila.indexOf(undefined)
+        if (posicao === -1) { // Se não tiver espaço na fila
+            console.log("Não tem espaço")
+            return
+        }
     fila[posicao] = item 
-    
-    if(posicao = fila.lastIndexOf(undefined)){
-        
-    }
+    return
 }
-
+inserirNaFila()
 
 // Retirar da fila --> remover(fila:any[]):any[] --> passa um parâmetro:
 // fila que é uma array de qualquer coisa; e retorna qualquer coisa
-function removerDaFila(fila){ 
-
+export function removerDaFila(fila){ 
+    if(fila[0] === undefined){
+        console.log("Fila Vazia")
+        return
+    }
+    fila.pop([0])
 }
+removerDaFila()
